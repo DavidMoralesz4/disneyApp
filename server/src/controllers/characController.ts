985c77314e5a231db3f6f99cca369944c3f1fe93
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-04 14:28:59 -0500

    controller: filtrado por campos -name-age- de req.query

--- server/src/controllers/characController.ts
+++ server/src/controllers/characController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
-import { createCharService, deleteCharService, detailCharService, getCharService, updateCharService } from "../services/characService";
+import {
+  createCharService,
+  deleteCharService,
+  detailCharService,
+  getCharService,
+  searchCharService,
+  updateCharService,
+} from "../services/characService";
 
-export const getCharController = async (_: Request, res: Response) => {
-  try {
+export const getCharController = async (req: Request, res: Response) => {
+    try {
     const characters = await getCharService();
+
     res.status(200).json(characters);
   } catch (error: unknown) {
     if (error instanceof Error) {
@@ -12,60 +20,67 @@ export const getCharController = async (_: Request, res: Response) => {
   }
 };
 
-export const detailCharController = async(_: Request, res: Response) => {
-    try {
-        const charactersDetail = await detailCharService()
-
-        res.status(200).json(charactersDetail)
-    } catch (error: unknown) {
-        if(error instanceof Error) {
-            res.status(500).json({message: error.message})
-        }
-    }
-}
-
-
-export const createCharController = async(req: Request, res: Response) => {
-  const { name, image, age, weight, history } = req.body;
+export const detailCharController = async (_: Request, res: Response) => {
   try {
-    await createCharService({name, image, age, history, weight })
+    const charactersDetail = await detailCharService();
 
-    res.status(200).json({message: 'El personaje se creo con exito!'})
+    res.status(200).json(charactersDetail);
   } catch (error: unknown) {
-    if(error instanceof Error) {
-        res.status(500).json({message: error.message})
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
     }
   }
 };
 
-
-export const updateCharController = async (req: Request, res: Response) => {
-    const {name, image, age, weight, history} = req.body /// Parametros que envia un cliente
-    const {id} = req.params // Id del character
-
-    try {
-    await updateCharService(id, {name, image, age, weight, history})
-    res.status(200).json({message: 'Personaje actualizado con exito'})     
+export const searchCharController = async(req: Request, res: Response) => {
+    const {name, age, movie} = req.query
+    
+    try {   
+        const characFilter = await searchCharService(name, age)
+        res.status(200).json(characFilter)
     } catch (error: unknown) {
-        if(error instanceof Error) {
-            res.status(500).json(error.message)
-        }
-    }
-}
-
-
-export const deleteCharController = async (req: Request, res: Response) => {
-    const {id} = req.params
-
-    try {        
-        await deleteCharService(id)
-        res.status(200).json({message: 'Personaje elimado con exito!'})
-    } catch (error: unknown) {
-        if(error instanceof Error) {
+        if(error instanceof Error){
             res.status(500).json({message: error.message})
         }
     }
 }
 
+export const createCharController = async (req: Request, res: Response) => {
+  const { name, image, age, weight, history } = req.body;
+  try {
+    await createCharService({ name, image, age, history, weight });
 
+    res.status(200).json({ message: "El personaje se creo con exito!" });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+};
 
+export const updateCharController = async (req: Request, res: Response) => {
+  const { name, image, age, weight, history } = req.body; /// Parametros que envia un cliente
+  const { id } = req.params; // Id del character
+
+  try {
+    await updateCharService(id, { name, image, age, weight, history });
+    res.status(200).json({ message: "Personaje actualizado con exito" });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json(error.message);
+    }
+  }
+};
+
+export const deleteCharController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    await deleteCharService(id);
+    res.status(200).json({ message: "Personaje elimado con exito!" });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+};

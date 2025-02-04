f24c792f22bd57a17859c41e70a42dd06407f28e
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-04 16:31:39 -0500

    service: getMovieServie

---
+++ server/src/services/movieService.ts
@@ -0,0 +1,19 @@
+import { Movies } from "../models/Movies";
+
+export const getMovieService = async () => {
+  try {
+    const movies = await Movies.findAll({
+      attributes: ["id", "title", "image", "date_release"],
+    });
+
+    if (!movies) {
+      throw new Error("No hay peliculas");
+    }
+
+    return movies;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};

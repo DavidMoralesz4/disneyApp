a3b91e52b10f096b91a9e17c7bf52e736b942b61
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-04 07:22:58 -0500

    CreateCharController: Controlador para crear un character

--- server/src/controllers/characController.ts
+++ server/src/controllers/characController.ts
@@ -1,11 +1,28 @@
-import { Request, RequestHandler, Response } from "express";
-import { getCharService } from "../services/characService";
+import { Request, Response } from "express";
+import { createCharService, getCharService } from "../services/characService";
 
-export const getCharController: RequestHandler = async(req: Request, res: Response) => {
-    try {
-        const characters = await getCharService()
-        res.status(200).json(characters)
-    } catch (error: any) {  
-        res.status(500).json({error: error.message})
+export const getCharController = async (_: Request, res: Response) => {
+  try {
+    const characters = await getCharService();
+    res.status(200).json(characters);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
     }
-}
+  }
+};
+
+export const createCharController = async(req: Request, res: Response) => {
+  const { name, image, age, weight, history } = req.body;
+  try {
+    await createCharService({name, image, age, history, weight })
+
+    res.status(200).json({message: 'El personaje se creo con exito!'})
+  } catch (error: unknown) {
+    if(error instanceof Error) {
+        res.status(500).json({message: error.message})
+    }
+  }
+};
+
+

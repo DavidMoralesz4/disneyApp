c24f1753b42cfadbabb85eb4e1aee1ac2ab77646
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-04 07:24:26 -0500

    endpoind: POST/characters

--- server/src/routes/characRoutes.ts
+++ server/src/routes/characRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getCharController } from "../controllers/characController";
+import { createCharController, getCharController } from "../controllers/characController";
 
 
 export const characRoutes: Router = Router()
 
 
 characRoutes.get('/characters', getCharController)
+
+characRoutes.post('/characters', createCharController)

bcbd078f2b2d9c7d912d3e841de26fa1b0c030e2
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-05 16:55:05 -0500

    endpoint: PUT/movies/:id actualiza propiedades de peliculas

--- server/src/routes/movieRoutes.ts
+++ server/src/routes/movieRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createMovieController, detailMovieController, getMovieController, searchMovieController } from "../controllers/movieController";
+import { createMovieController, detailMovieController, getMovieController, searchMovieController, updateMovieController } from "../controllers/movieController";
 
 export const movieRouter = Router();
 
@@ -10,3 +10,5 @@ movieRouter.get('/movies/:id', detailMovieController)
 movieRouter.get('/movies/search/mov', searchMovieController)
 
 movieRouter.post('/movies', createMovieController)
+
+movieRouter.put('/movies/:id', updateMovieController)

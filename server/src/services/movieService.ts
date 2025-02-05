b99eca25e9229ccde23e816681e5cea7dd5b3f3a
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-05 14:58:37 -0500

    service: Filtrar titulo y fecha ASC

--- server/src/services/movieService.ts
+++ server/src/services/movieService.ts
@@ -1,3 +1,4 @@
+import { Op } from "sequelize";
 import { Character, Movies } from "../models/Models";
 
 export const getMovieService = async () => {
@@ -18,24 +19,53 @@ export const getMovieService = async () => {
   }
 };
 
+export const detailMovieService = async (id: string) => {
+  try {
+    const movieDetail = await Movies.findByPk(id, {
+      include: {
+        model: Character, // Usamos Movie (singular)
+        attributes: ["name"],
+      },
+    });
 
-export const detailMovieService = async(id: string) => {
-    try {
-      const movieDetail = await Movies.findByPk(id, {
-        include: {
-          model: Character, // Usamos Movie (singular)
-          attributes: ["name"],
+    if (!movieDetail) {
+      throw new Error("Hubo un problema en los detalles");
+    }
+
+    return movieDetail;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+export const searchMovieService = async (title: any, date_release: any) => {
+  try {
+
+    const allDates = await Movies.findAll();
+    
+    
+    if(title !== undefined ) {
+      return await Movies.findAll({
+        attributes: ["id", "title", "image", "date_release"],
+        where: {
+          [Op.or]: [{ title: title }],
         },
       });
-  
-      if (!movieDetail) {
-        throw new Error("Hubo un problema en los detalles");
-      }
-  
-      return movieDetail;
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
     }
-}
+
+    if (date_release !== undefined) {
+      return allDates
+        .filter((values) => values.date_release >= date_release)
+        .sort((a: any, b: any) => a.date_release - b.date_release);
+    }
+
+  
+    // return movieFiltered;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};

a2b6ad91cadaf562919bba1b4b908885ce41a6f4
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-05 16:56:28 -0500

    controller: updateMovieController extraer propiedades de req.body

--- server/src/controllers/movieController.ts
+++ server/src/controllers/movieController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createMovieService, detailMovieService, getMovieService, searchMovieService } from "../services/movieService";
+import { createMovieService, detailMovieService, getMovieService, searchMovieService, updateMovieService } from "../services/movieService";
 
 export const getMovieController = async (_: Request, res: Response) => {
   try {
@@ -23,8 +23,8 @@ export const detailMovieController = async (req: Request, res: Response) => {
   } catch (error: unknown) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
-    }
-  }
+    };
+  };
 };
 
 
@@ -36,13 +36,14 @@ export const searchMovieController = async(req: Request, res: Response) => {
         res.status(200).json(movieFiltered)
     } catch (error: unknown) {
         if(error instanceof Error){
-            res.status(500).json({message: error.message})
+            res.status(500).json({message: error.message});
         }
     }
 }
 
 export const createMovieController = async (req: Request, res: Response) => {
   const { title, image, date_release, score } = req.body;
+  
   try {
     await createMovieService({ title, image, date_release, score });
 
@@ -53,3 +54,17 @@ export const createMovieController = async (req: Request, res: Response) => {
     }
   }
 };
+
+export const updateMovieController = async(req: Request, res: Response) => {
+    const { title, image, date_release, score } = req.body; /// Parametros que envia un cliente
+    const { id } = req.params; // Id de la pelicula
+  
+    try {
+      await updateMovieService(id, { title, image, date_release, score });
+      res.status(200).json({ message: "Pelicula actualizada con exito!" });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        res.status(500).json(error.message);
+      }
+    }
+}

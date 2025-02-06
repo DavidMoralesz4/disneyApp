1a36bc48dc1a6e16db48fca0a439297bf8c82fe4
DavidMoralesz4 <jd3285201@gmail.com>
2025-02-05 21:24:44 -0500

    controller: loginController extrae token del servicio - uso de las cookies

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,14 +1,28 @@
 import { Request, Response } from "express";
-import { registerService } from "../services/authService";
+import { loginService, registerService } from "../services/authService";
+import jwt from "jsonwebtoken";
 
-export const registerController = async(req: Request, res: Response) => {
-    const {username, email, password} = req.body
-    try {
-        await registerService(username, email, password)
-        res.status(200).json({message: 'Usuario registrado con exito!'})
-    } catch (error: unknown) {
-        if(error instanceof Error){
-            res.status(500).json({message: error.message})
-        }
+export const registerController = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+  try {
+    await registerService(username, email, password);
+    res.status(200).json({ message: "Usuario registrado con exito!" });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
     }
-}
+  }
+};
+
+export const loginController = async(req: Request, res: Response): Promise<any> => {
+    const {email, password} = req.body
+    try {
+        const {user, token} = await loginService(email, password)        
+        res.cookie('access_token', token)
+        res.status(200).json({user})
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+};
